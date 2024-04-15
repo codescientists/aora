94cc9d94cc4755d5ae171caa43eca3500c688791
codescientists <piyushpp2412@gmail.com>
2024-04-15 20:09:43 +0530

    Added app icon and splash screen

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,11 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
+import React, { useEffect } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import SearchInput from '../components/SearchInput'
+import EmptyState from '../components/EmptyState'
+import { getUserPosts, signOut } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../components/VideoCard'
+
+import { useGlobalContext } from '../../context/GlobalProvider'
+import { icons } from '../../constants'
+import InfoBox from '../components/InfoBox'
+import { router } from 'expo-router'
 
 const Bookmark = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext()
+  const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
+
+  const logout = async () => {
+    await signOut();
+    setUser(null)
+    setIsLoggedIn(false)
+
+    router.replace('/sign-in')
+  }
+
   return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList 
+        data={posts}
+        keyExtractor={(item) => item.id}
+        renderItem={( {item} ) => (
+          <VideoCard video={item}/>
+        )}
+        ListHeaderComponent={() => (
+          <View className="w-full items-center justify-center mt-6 mb-12 px-4">
+            <SearchInput /> 
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this search query"
+          />
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
